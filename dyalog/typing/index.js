cfcbd5f6326f378fb6ce67eb239f000df23485d7
awagga <awagga@mail.com>
2022-01-21 23:51:04 +0000

    Add modern glyhps, ⎕AV is outdated

--- dyalog/typing/index.js
+++ dyalog/typing/index.js
@@ -1,3 +1,3 @@
-const av = `⌶⍺⍵¯⊢⊣⌷¨⍱⍨⌿⍀≤≥≠∨∧÷×∊⍴↑↓⍳○⌈⌊∇∘⊂⊃∩∪⊥⊤⍲⍒⍋⍉⌽⊖⍟⌹⍕⍎⍫⍪≡≢ö@∣⍷⋄←→⍝⎕⍞⍣`
+const av = '¨¯×÷←↑→↓∆∇∊∘∧∨∩∪≠≡≢≤≥⊂⊃⊆⊖⊢⊣⊤⊥⋄⌈⌊⌶⌷⌸⌹⌺⌽⌿⍀⍉⍋⍎⍒⍕⍙⍝⍞⍠⍣⍤⍥⍨⍪⍫⍬⍱⍲⍳⍴⍵⍷⍸⍺⎕'
 
 for (let i = 0; i < 1000; i++) { document.getElementById("t").textContent += av[Math.floor(Math.random() * av.length)]}
