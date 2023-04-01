1ecb51a103eb33b6e0ea81eb070b20d3c51251d7
finooiigee <essie@lovett.gay>
2023-04-01 02:02:10 +0100

    restructure repository

--- dyalog/typing/index.js
+++
@@ -1,3 +0,0 @@
-const av = '¨¯×÷←↑→↓∆∇∊∘∧∨∩∪≠≡≢≤≥⊂⊃⊆⊖⊢⊣⊤⊥⋄⌈⌊⌶⌷⌸⌹⌺⌽⌿⍀⍉⍋⍎⍒⍕⍙⍝⍞⍠⍣⍤⍥⍨⍪⍫⍬⍱⍲⍳⍴⍵⍷⍸⍺⎕'
-
-for (let i = 0; i < 1000; i++) { document.getElementById("t").textContent += av[Math.floor(Math.random() * av.length)]}
